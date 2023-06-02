c016ef3967bc6e82128e0b25cbae89331ac284b7
Dwika <dwikariswanda@gmail.com>
2023-06-03 02:16:25 +0700

    Config Swagger

--- src/main/java/com/spring/binar/challenge_5/Challenge5Application.java
+++ src/main/java/com/spring/binar/challenge_5/Challenge5Application.java
@@ -5,9 +5,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableSwagger2
 public class Challenge5Application {
 
 	@Bean
